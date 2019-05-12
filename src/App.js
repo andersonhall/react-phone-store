6ade8dcc60b45147613b8f00861893312f8eaf80
Andy Hall <40648667+andersonhall@users.noreply.github.com>
2019-05-12 09:06:56 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Details from './components/Details';
-import Cart from './components/Cart';
+import Cart from './components/Cart/Cart';
 import Default from './components/Default';
 import Modal from './components/Modal';
 
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={ProductList} />
-          <Route path="/details" component={Details} />
-          <Route path="/cart" component={Cart} />
-          <Route component={Default} />
-        </Switch>
-        <Modal />
-      </React.Fragment>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" component={ProductList} />
+					<Route path="/details" component={Details} />
+					<Route path="/cart" component={Cart} />
+					<Route component={Default} />
+				</Switch>
+				<Modal />
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
